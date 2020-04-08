2b8aa190ac994306c33404ebb89f977550707468
Glitch (aat38-assignment3b) <none>
2020-04-08 11:45:59 +0000

    Rewound to commit 90d0037398afe8783d27f4987280aa971a47669b + 0 edits

--- server.js
+++ server.js
@@ -1,24 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
-const path = require('path');
 
-app.use(express.static('public'));//use the static files in the public folder
-app.set("views", __dirname + "/views/");//tell express where to get views and which template engine to use
-app.set("view engine", "ejs");
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); 
+// make all the files in 'public' available
+// https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
 
-
-//----------------------------ROUTES-------------------------------------
-
-app.get('/', function(request, response) {
-  response.render("index")
+// https://expressjs.com/en/starter/basic-routing.html
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 
 
-//----------------------------ROUTER-------------------------------------
+// listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });
