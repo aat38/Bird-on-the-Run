210c9c58cfda336e69bcb563ddaa0e814c8462d4
Glitch (aat38-assignment3b) <none>
2020-04-09 15:49:31 +0000

    🌒💃 Checkpoint ./partials/bootstrap.js:10221761/9593 ./views/index.ejs:10221761/184

--- partials/bootstrap.js
+++ partials/bootstrap.js
@@ -1,9 +1,10 @@
-    <link
+<div>
+  <link
       rel="stylesheet"
       href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
       integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
       crossorigin="anonymous"
-    />
+     ></link>
     <script
       src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
       integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -19,4 +20,6 @@
       integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
       crossorigin="anonymous"
     ></script>
-    <link rel="stylesheet" href="/projectsStyle.css" />
+</div> 
+     
+     
