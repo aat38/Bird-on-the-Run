8f99c7fa73464d5d28dde8197ca1014e8be50c5e
Glitch (aat38-assignment3b) <none>
2020-04-09 00:36:31 +0000

    ⛩🚘 Checkpoint ./routes/index.js:10221761/3409

--- routes/index.js
+++ routes/index.js
@@ -8,33 +8,20 @@ const Food = require("../models/food");
 
 // RETREIVE all books
 router.get("/", function(req, res) {
-  Food.find({}, function(err, food_list) {
+  Food.find({}, 'item', function(err, food_list) {
     // console.log(food_list)
     // res.render("index", { food: food_list });
     
     var query = Food.find({}).select('item -_id');
 
-    query.exec(function (err, food_names) {
+    .exec(function (err, food_names) {
         if (err) return (err);
-        res.render("index", { food: (food_names) });
+        res.render("index", { food: food_names });
         // res.send(food_names);
     });  
     
   });
 
 });
-
-router.get('/getFriends', function(req, res) {
-  Food.find({}).select('item -_id');
-
-  query.exec(function(err, food_names)) {
-    var friends = food_names.map(function(v) {
-      delete(v._id);
-      delete(v.status);
-      return v;
-    });
-    res.json(friends);
-  }) 
-})
 
 module.exports = router;
